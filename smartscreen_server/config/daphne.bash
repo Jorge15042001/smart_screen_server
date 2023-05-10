5e9e0123b487288a16292d5d9812b4a313cda352
Jorge Vuglarin <adrivulgarin@hotmail.com>
2023-05-10 06:56:49 +0000

    changes

--- smartscreen_server/config/daphne.bash
+++ smartscreen_server/config/daphne.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/jorge1504/projects/smartscreen-server/
+cd /root/smart_screen_server
 export DJANGO_SETTINGS_MODULE=smartscreen_server.settings
-pipenv run bash -c "cd smartscreen_server;daphne -b 0.0.0.0 -p 8001 smartscreen_server.asgi:application"
+pipenv run bash -c "cd smartscreen_server;daphne -e ssl:8001:privateKey=/etc/letsencrypt/live/smartscreen.jorgevulgarin.cc/privkey.pem:certKey=/etc/letsencrypt/live/smartscreen.jorgevulgarin.cc/fullchain.pem smartscreen_server.asgi:application"
+#pipenv run bash -c "cd smartscreen_server;daphne -b 0.0.0.0 -p 8001 smartscreen_server.asgi:application"
+
