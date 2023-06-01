ba1cef8a224d0753e78480e25ba2950e964b55ca
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-06-01 00:47:35 -0500

    in memory channels

--- smartscreen_server/smartscreen_server/settings.py
+++ smartscreen_server/smartscreen_server/settings.py
@@ -89,10 +89,11 @@ WSGI_APPLICATION = 'smartscreen_server.wsgi.application'
 ASGI_APPLICATION = 'smartscreen_server.asgi.application'
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        #  "BACKEND": "channels_redis.core.RedisChannelLayer",
+        #  "CONFIG": {
+        #      "hosts": [("127.0.0.1", 6379)],
+        #  },
     },
 }
 
