6e0c354725cc8a885fe198f26cafaa7e812a8c42
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-06-07 22:51:23 -0500

    deployment files

--- smartscreen_server/config/daphne.bash
+++ smartscreen_server/config/daphne.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd /root/smart_screen_server
+cd /home/manager/smart_screen_server
 export DJANGO_SETTINGS_MODULE=smartscreen_server.settings
 #pipenv run bash -c "cd smartscreen_server;daphne -e ssl:8001:privateKey=/etc/letsencrypt/live/smartscreen.jorgevulgarin.cc/privkey.pem:certKey=/etc/letsencrypt/live/smartscreen.jorgevulgarin.cc/fullchain.pem smartscreen_server.asgi:application"
+pwd
 pipenv run bash -c "cd smartscreen_server;daphne -b 0.0.0.0 -p 8001 smartscreen_server.asgi:application"
 
