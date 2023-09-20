4af1efa70ae1b0af74c4bb56ab53d30bd3050578
Jorge10542001 <adrivuglarin@hotmail.com>
2023-09-20 09:36:19 -0500

    quickfix

--- smartscreen_server/smartscreen/static/smartscreen/call_popup.js
+++ smartscreen_server/smartscreen/static/smartscreen/call_popup.js
@@ -1,26 +1,32 @@
-class CallPopup{
-  constructor(screen_id){
+class CallPopup {
+  constructor(screen_id) {
     this.screen_id = screen_id;
     this.window = null;
   }
-  open(){
+  open() {
     const call_window = window.open(`/receptionist_screen/${this.screen_id}`, "_blank", "toolbar=no,scrollbars=no,resizable=yes,menubar=no,titlebar=no,location=no,status=no");
     this.window = call_window;
+
+    setTimeout(() => {
+      this.window.document.getElementById("end_call").onclick = () => {
+        this.close();
+      }
+    }, 3000);
     return call_window;
   }
-  close(){
+  close() {
     this.window.close();
   }
-  is_opened(){
-    return this.window === null || !this.window.closed 
+  is_opened() {
+    return this.window === null || !this.window.closed
   }
 }
-class CallPopupManager{
-  constructor(){
+class CallPopupManager {
+  constructor() {
     this.popup = null;
   }
-  newPopup(screen_id){
-    if (this.popup!==null && this.popup.is_opened()){
+  newPopup(screen_id) {
+    if (this.popup !== null && this.popup.is_opened()) {
       throw new Error("There is an existing popup");
     }
     this.popup = new CallPopup(screen_id);
